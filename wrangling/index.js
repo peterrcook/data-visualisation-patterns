00e75ff902c2ec1ec1b9a2ba2e8282f7801183c7
Peter Cook <peter@peterrcook.com>
2020-02-09 13:33:40 +0000

    Wrangling

---
+++ wrangling/index.js
@@ -0,0 +1,14 @@
+let d3 = require('d3');
+let fs = require('fs');
+
+let f = fs.readFileSync('films.csv', 'utf8');
+let data = d3.csvParse(f);
+
+let nested = d3.nest()
+    .key(d => d['Lead Studio'])
+    .key(d => d['Genre'])
+    .key(d => d['Film'])
+    .rollup(v => d3.sum(v, d => +d['Worldwide Gross ($m)']))
+    .entries(data);
+
+fs.writeFileSync('data.json', JSON.stringify(nested, null, 2));
