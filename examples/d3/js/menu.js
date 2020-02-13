fcdeb1ee1b0825727eb546a36a2ddca7be587c79
Peter Cook <peter@peterrcook.com>
2020-02-13 09:31:31 +0000

    Add menu.js

---
+++ examples/d3/js/menu.js
@@ -0,0 +1,42 @@
+var menuItems = [
+    {
+        id: 'country',
+        label: 'Country'
+    },
+    {
+        id: 'renewable',
+        label: 'Renewable'
+    },
+    {
+        id: 'hydroelectric',
+        label: 'Hydroelectric'
+    },
+    {
+        id: 'oilgascoal',
+        label: 'Oil, Gas & Coal'
+    },
+    {
+        id: 'nuclear',
+        label: 'Nuclear'
+    }
+];
+
+function updateMenu() {
+    d3.select('#menu .items')
+        .selectAll('.item')
+        .data(menuItems)
+        .join('div')
+        .classed('item', true)
+        .classed('selected', function(d) {
+            return sortBy === d.id;
+        })
+        .html(function(d) {
+            return '<svg width="18" height="18"><circle class="' + d.id + '" cx="9" cy="9" r="8"></svg><div class="label">' + d.label + '</div>';
+        })
+        .on('click', function(d) {
+            sortBy = d.id;
+            doSort();
+            update();
+            updateMenu();
+        });
+}
