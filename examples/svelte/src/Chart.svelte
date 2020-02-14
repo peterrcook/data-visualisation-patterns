b61aa27665a51b267705f3a455c3dda84fbf0c14
Peter Cook <peter@peterrcook.com>
2020-02-14 17:12:42 +0000

    Svelte: simplify store logic in Chart component
    Using https://svelte.dev/tutorial/auto-subscriptions

--- examples/svelte/src/Chart.svelte
+++ examples/svelte/src/Chart.svelte
@@ -9,25 +9,20 @@ import getLayout from './layout';
 
 export let data, width = 1200, numColumns = 14;
 
-let sortByValue;
-
 $: radiusScale = scaleSqrt().domain([0, 100]).range([0, 0.35 * width / numColumns]);
-$: sortedData = sortData(data, sortByValue);
+$: sortedData = sortData(data, $selectedSortBy);
 $: layout = getLayout(sortedData, {
   width,
   numColumns,
   radiusScale,
-  sortBy: sortByValue
+  sortBy: $selectedSortBy
 });
 
-const unsubscribe = selectedSortBy.subscribe(value => {
-  sortByValue = value;
-});
 </script>
 
 
 <div id="chart-wrapper">
-  <svg width="1200" height="1150">
+  <svg width={width} height="1150">
     <g id="chart">
       {#each layout as d, i (d.id)}
         <CircleGroup
